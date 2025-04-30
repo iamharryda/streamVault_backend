71f87f1acc4eaf473c0a4eb3a8ebb4a3393769f2
oliviafarrand <olivia.farrand@tutors.es>
2025-04-30 12:45:38 +0300

    Landing imported to index.tsx

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
+import Landing from './pages/Auth/Landing';
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Landing/>
     </View>
   );
 }
