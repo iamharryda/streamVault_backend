aa1c336182320cdd48b85f619de3d16e3529b8ed
oliviafarrand <olivia.farrand@tutors.es>
2025-05-08 14:37:59 +0300

    styling

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,27 +1,59 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
-
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Navbar = () => {
 
-    const [isPressed, setIsPressed] = useState(false);
-
-    const handlePress = () => { 
-        setIsPressed(!isPressed);
-    };
-
     return (
         <View style= {styles.container}>
-            <Text style= {styles.text}></Text>
-            <Pressable onPress={handlePress}>
-                <Text style={[styles.text,  isPressed && styles.textPressed]}>trending</Text>
-            </Pressable>
-            <Pressable onPress={handlePress}>
-                <Text style={[styles.text,  isPressed && styles.textPressed]}>search</Text>
-            </Pressable>
-            <Pressable onPress={handlePress}>
-                <Text style={[styles.text,  isPressed && styles.textPressed]}>wishlist</Text>
-            </Pressable>
+            <View style= {styles.pfBorder}>
+                <TouchableOpacity style={styles.profile}>
+                    <image href="" style={styles.pfp}></image>
+                    <View style={styles.pfFlex}>
+                        <Text style={styles.name}>John Doe</Text>
+                        <Text style={styles.place}>Dublin, Ireland</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style = {styles.trending}>Trending</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Search</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Watchlist</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Recently watched</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Lists</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Activity</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Language & Region</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Settings</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>About StreamVault</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listFlex}>
+                <image href="" style={styles.icon}></image>
+                <Text style={styles.text}>Sign Out</Text>
+            </TouchableOpacity>
         </View>
       );
 }
@@ -30,12 +62,58 @@ export default Navbar
 const styles = StyleSheet.create ({
     container: {
         backgroundColor: '#0D282F',
+        width: 299,
+        flex: 1,
+        flexDirection: 'column',
     }, 
+    icon: {
+        height: 19, width: 19,
+    },
     text: {
         color: '#B1B8B9',
-        
+        height: 44,
+        fontWeight: '500',
+        fontSize: 14,
     },
-    textPressed: {
+    trending: {
         color: '#FACC15',
-    }
+        height: 44,
+        fontWeight: '500',
+        fontSize: 14,
+    },
+    listFlex: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+
+    pfFlex: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    profile: {        
+        flex: 1,
+        flexDirection: 'row',
+    },
+    pfBorder: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#36474B',
+        opacity: 30,
+    },
+    pfp: {
+        width: 56,
+        height: 56,
+        backgroundColor: 'white',
+        borderRadius: 100,
+        marginLeft: 16, marginRight: 16,
+    },
+    name: {
+        color: '#E6E8E9',
+        fontWeight: '700',
+        fontSize: 18,
+    },
+    place: {
+        color: '#8C9598',
+        fontWeight: '400',
+        fontSize: 12,
+    },
 })
