a0b77614cb2bc90833d8304f075ac735e868d9c2
oliviafarrand <olivia.farrand@tutors.es>
2025-05-09 15:28:27 +0300

    Icons and styling mostly done

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
+// https://icons.expo.fyi/Index
 
 const Navbar = () => {
 
@@ -14,44 +18,44 @@ const Navbar = () => {
                     </View>
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+            <TouchableOpacity style={styles.trendingFlex}>
+                <Ionicons name="flame" style={styles.trendingIcon}/>
                 <Text style = {styles.trending}>Trending</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="search1" style={styles.icon}/>
                 <Text style={styles.text}>Search</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="laptop" style={styles.icon}/>
                 <Text style={styles.text}>Watchlist</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="clockcircleo" style={styles.icon}/>
                 <Text style={styles.text}>Recently watched</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="bars" style={styles.icon}/>
                 <Text style={styles.text}>Lists</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <Feather name="activity" style={styles.icon}/>
                 <Text style={styles.text}>Activity</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="find" style={styles.icon}/>
                 <Text style={styles.text}>Language & Region</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="setting" style={styles.icon}/>
                 <Text style={styles.text}>Settings</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="infocirlceo" style={styles.icon}/>
                 <Text style={styles.text}>About StreamVault</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listFlex}>
-                <image href="" style={styles.icon}></image>
+                <AntDesign name="logout" style={styles.icon}/>
                 <Text style={styles.text}>Sign Out</Text>
             </TouchableOpacity>
         </View>
@@ -62,36 +66,51 @@ export default Navbar
 const styles = StyleSheet.create ({
     container: {
         backgroundColor: '#0D282F',
-        width: 299,
-        flex: 1,
+        width: 299, minHeight: 700,
         flexDirection: 'column',
     }, 
     icon: {
-        height: 19, width: 19,
-    },
-    text: {
         color: '#B1B8B9',
-        height: 44,
-        fontWeight: '500',
-        fontSize: 14,
+        marginLeft: 16, marginTop: 10, marginBottom: 10,
+        fontSize: 19,
     },
-    trending: {
+    trendingIcon: {
         color: '#FACC15',
-        height: 44,
+        marginLeft: 16, marginTop: 10, marginBottom: 10,
+        fontSize: 19,
+    },
+    text: { 
+        color: '#B1B8B9',
+        height: 20,
         fontWeight: '500',
         fontSize: 14,
+        paddingLeft: 18, paddingTop: 12, paddingBottom: 12,
+    },
+    trending: { 
+        color: '#FACC15',
+        height: 20,
+        fontWeight: '500',
+        fontSize: 14,
+        paddingLeft: 18, paddingTop: 12, paddingBottom: 12, 
+    },
+    trendingFlex: {
+        flex: 1,
+        flexDirection: 'row',
+        height: 44,
+        marginBottom: 8, marginTop: 24,
     },
     listFlex: {
         flex: 1,
         flexDirection: 'row',
+        height: 44,
+        marginBottom: 8,
+
     },
 
     pfFlex: {
-        flex: 1,
         flexDirection: 'column',
     },
     profile: {        
-        flex: 1,
         flexDirection: 'row',
     },
     pfBorder: {
@@ -105,11 +124,13 @@ const styles = StyleSheet.create ({
         backgroundColor: 'white',
         borderRadius: 100,
         marginLeft: 16, marginRight: 16,
+        marginTop: 40, marginBottom: 42,
     },
     name: {
         color: '#E6E8E9',
         fontWeight: '700',
         fontSize: 18,
+        marginTop: 46,
     },
     place: {
         color: '#8C9598',
