152c0e883130acf3cf4b7f1fcc93d5affc48f285
TheVeksel <masxlesfrom@gmail.com>
2025-09-25 15:23:18 +0300

    Types refactoring, Search bars code improvement

--- frontend/app/(tabs)/Discover.tsx
+++ frontend/app/(tabs)/Discover.tsx
@@ -1,22 +1,15 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, TouchableOpacity, Text } from "react-native";
+import { ScrollView } from "react-native";
 import { getTrendingMovies, getNowPlayingMovies } from "@/src/api/tmdb";
 import { useEffect, useState } from "react";
 import MovieSection from "@/src/components/MovieSection";
-import { Ionicons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
+import SearchButton from "@/src/components/SearchButton";
+import { IMovie } from "@/src/types/interfaces/IMovie";
 
-type Movie = {
-  id: number;
-  poster_path: string;
-  title: string;
-  release_date: string;
-};
 
 export default function Discover() {
-  const router = useRouter();
-  const [trendingMovies, setTrendingMovies] = useState<Movie[]>([]);
-  const [latestMovie, setLatestMovie] = useState<Movie[]>([]);
+  const [trendingMovies, setTrendingMovies] = useState<IMovie[]>([]);
+  const [latestMovie, setLatestMovie] = useState<IMovie[]>([]);
 
   useEffect(() => {
     searchMovies();
@@ -27,28 +20,20 @@ export default function Discover() {
     setTrendingMovies(trending);
     const latest = await getNowPlayingMovies();
     setLatestMovie(latest);
-  }
+  };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#04191E' }}>
-      <ScrollView style={{ gap: 20, marginTop: 20 }}>
-        <TouchableOpacity onPress={() => router.push('./SearchScreen')} style={{ borderRadius: 8, flexDirection: 'row', alignItems: 'center', alignSelf: 'center', paddingVertical: 12, marginBottom: 20, width: '75%', backgroundColor: '#0d282f' }}>
-          <Ionicons name="search" size={20} color="#e6e8e9" style={{ marginHorizontal: 10}} />
-          <Text style={{ color: '#e6e8e9', marginLeft: 10 }}>Search</Text>
-        </TouchableOpacity>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#04191E" }}>
+      <ScrollView>
+        <SearchButton></SearchButton>
 
         <MovieSection
           sectionTitle="Trending this week"
           movies={trendingMovies}
         />
 
-        <MovieSection 
-          sectionTitle="New Releases" 
-          movies={latestMovie} 
-        />
-
+        <MovieSection sectionTitle="New Releases" movies={latestMovie} />
       </ScrollView>
     </SafeAreaView>
   );
 }
-     
