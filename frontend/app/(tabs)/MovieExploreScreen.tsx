58778cbfe4de869e7702cfdcc0d22c1eacb59933
Amoah <emmanuel.amoah@tutors.es>
2025-10-24 07:53:00 +0000

    Add MovieExploreScreen component
    The MovieExploreScreen.tsx imports the MovieExplore.tsx

---
+++ frontend/app/(tabs)/MovieExploreScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import MovieExplore from '@/src/components/MovieExplore'
+
+export default function MovieExploreScreen () {
+  return (
+    <View>
+      <MovieExplore />
+    </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({})
